d92093ab1b470cf1c23aeae87fdfd7a0036b96ec
JitenKumar <jitenderpalsra@gmail.com>
2018-03-31 18:58:25 +0530

    Enumerate in python

---
+++ Built In Functions/Enumerate/enumerate.py
@@ -0,0 +1,9 @@
+for count,item in enumerate([1,23,4,4555,45]):
+    print(count)
+    print(item)
+
+for index,item in enumerate([1,2,34,4,5]):
+    if index>3:
+        print(item)
+    else:
+        print('Not Countable')
