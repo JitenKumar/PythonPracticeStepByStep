da97ce0d63e84f8deae2202661df65f46609054c
JitenKumar <jitenderpalsra@gmail.com>
2018-03-31 14:35:27 +0530

    Exception handling in python

---
+++ Errors and Exceptions Handling/Pylint/main.py
@@ -0,0 +1,19 @@
+'''
+Pylint overvies
+To install pylint
+use pip install pylint
+then use pylint main.py on the file you want to work with in the terminal
+'''
+
+def func():
+
+    '''
+    this is a simple method
+    :return: sum
+    '''
+
+    var_a = 2
+    var_b = 3
+    print(var_a + var_b)
+
+func()
