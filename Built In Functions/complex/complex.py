193621a6c62353afd5bccd4575d22f9ac442cc49
JitenKumar <jitenderpalsra@gmail.com>
2018-03-31 19:05:51 +0530

    complex in python

---
+++ Built In Functions/complex/complex.py
@@ -0,0 +1,6 @@
+# omplex() returns a complex number with the value real + imag*1j or converts a string or number to a complex number.
+
+print(complex(3,1))
+print(complex(4,8274))
+print(complex('4+3j'))
+
