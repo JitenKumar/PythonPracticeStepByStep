244a08b023adb7f0a141cc3f15763606f1264100
JitenKumar <jitenderpalsra@gmail.com>
2018-04-02 00:47:21 +0530

    Advanced Numbers

---
+++ Advanced Python Objects and Data Structures/Advanced Numbers/numbers.py
@@ -0,0 +1,28 @@
+# HexamDecimal of numbers
+
+print(hex(123))
+print(hex(23))
+
+# Binary
+
+print(bin(16))
+print(bin(45))
+print(bin(129))
+print(bin(255))
+
+# Power functions
+
+print(pow(2,6))
+
+print(pow(3,4))
+
+# powe(x,y,z) = (x**y) % z
+pow(3,4,1)
+
+print(round(3.14324,2))
+print(round(3.13324))
+
+print(abs(12.3))
+print(round(3))
+
+print(abs(-2))
